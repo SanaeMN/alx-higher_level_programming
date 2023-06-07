b33649aa200e472e37693a10ff56c0e8915b3c39
SanaeMN <sanaeenemuom2017@gmail.com>
2023-06-06 18:20:53 -0700

    14. Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+nbr = ord('A') - ord('a')
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i + nbr)), end="")
