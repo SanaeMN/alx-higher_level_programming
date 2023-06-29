e7f23c1580902696c5f17ea6d6c4b32b74faaf24
SanaeMN <sanaeenemuom2017@gmail.com>
2023-06-29 15:11:13 -0700

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square file"""
+
+
+class Square:
+    """Represent a square class"""
+
+    def __init__(self, n=0):
+        """class constructor"""
+         if not isinstance(n, int):
+            raise TypeError('size must be an integer')
+        if n < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = n
