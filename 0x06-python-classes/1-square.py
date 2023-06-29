74ae583bd9d40e780717d99c3dcc848c2a2379c8
SanaeMN <sanaeenemuom2017@gmail.com>
2023-06-29 15:10:00 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square file"""
+
+
+class Square:
+    """Represent a square class"""
+
+    def __init__(self, n):
+        """class constructor"""
+        self.__size = n
