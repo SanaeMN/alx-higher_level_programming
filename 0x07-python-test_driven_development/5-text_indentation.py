6edcccf72ca1b26c13a672259985837c4ed90fdd
SanaeMN <sanaeenemuom2017@gmail.com>
2023-07-04 19:32:51 -0700

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function"""
+
+
+def text_indentation(text):
+    """
+    Print text with two new lines after each '.', '?', and ':'
+    :param text:
+    :type text: str
+    :return: none
+    :rtype: none
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
