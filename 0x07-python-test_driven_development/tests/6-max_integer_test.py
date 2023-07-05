3600d521a464d857f2051cbb90847779917b242f
SanaeMN <sanaeenemuom2017@gmail.com>
2023-07-04 19:36:42 -0700

    test file

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a unittest for max_integer([..])
+"""
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Unitest for max_integer([..]).
+    """
+
+    def test_equal(self):
+        """
+        all possible equal tests
+        :return:git
+        :rtype:
+        """
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(list(range(10))), 9)
+        self.assertEqual(max_integer([9, 1, 0, -19, 19]), 19)
+        self.assertEqual(max_integer([14.2, 26.1, 9]), 26.1)
+        self.assertEqual(max_integer(''), None)
+        self.assertEqual(max_integer('I love Python'), 'y')
+        self.assertEqual(max_integer(['I', 'love', 'Python']), 'love')
+
+
+if __name__ == '__main__':
+    unittest.main()
