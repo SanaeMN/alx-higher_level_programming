74b9d70cff1cf50bdc3849737b6c903c297955d2
SanaeMN <sanaeenemuom2017@gmail.com>
2023-07-11 19:09:11 -0700

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student"""
+        if type(attrs) == list and all(type(att) == str for att in attrs):
+            return {att: getattr(self, att) for att in attrs
+                    if hasattr(self, att)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student"""
+        for att, value in json.items():
+            setattr(self, att, value)
