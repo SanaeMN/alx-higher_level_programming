d491b645515ff71018d49f618ff85f8f1d2dd49e
SanaeMN <sanaeenemuom2017@gmail.com>
2023-07-11 19:10:41 -0700

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function"""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n."""
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        triangle.append([1 for _ in range(i + 1)])
+        for j in range(len(triangle[i])):
+            try:
+                if i < 1 or j < 1:
+                    raise IndexError
+                triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1]
+            except IndexError:
+                pass
+    return triangle
